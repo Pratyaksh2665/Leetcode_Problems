b709460c8311723d6932a04abec6ead7b986b328
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-06-30 09:09:25 +0530

    Time: 38 ms (13.27%), Space: 16.8 MB (7.79%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,39 +1,33 @@
-
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n=s.length();
-        int ans=0;
-        unordered_map<char,int>mp;
+        unordered_map<char , int>mp;
+        
         int i=0;
-        for(int j=0;j<n;j++)
+        int j=0;
+        int ans=0;
+       
+        while(j<n)
         {
             mp[s[j]]++;
+
             while(mp.size()==3)
+            {
+                ans+=(n-j);
+
+                mp[s[i]]--;
+                
+                if(mp[s[i]]==0)
                 {
-                    mp[s[i]]--;
-                    ans+=(n-j);
-                    if(mp[s[i]]==0) mp.erase(s[i]);
-                    i++;
+                    mp.erase(s[i]);
                 }
-         
+                i++;
+            }
+            j++;
+
 
         }
         return ans;
-        
-
     }
 };
-
-// these substrings are all valid:as contain a,b,as well as c
-
-// [i..j]
-// [i..j+1]
-// [i..j+2]
-// ...
-// [i..n-1]
-
-
-// Number of them:
-
-// n - j
