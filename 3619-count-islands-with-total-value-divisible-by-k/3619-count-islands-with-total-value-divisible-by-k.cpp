0b95d22aa7b535c7ac2de8e928a38fd4b20793d8
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-07-04 15:19:32 +0530

    Time: 36 ms (59.55%), Space: 90 MB (51.39%) - LeetHub

---
+++ 3619-count-islands-with-total-value-divisible-by-k/3619-count-islands-with-total-value-divisible-by-k.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+        void dfs(vector<vector<int>>& grid ,int i,int j ,vector<vector<int>>&vis ,long long &count)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(vis[i][j]) return;
+        count+=grid[i][j];
+        vis[i][j]=1;
+
+        int row[4]={-1,1,0,0};
+        int col[4]={0,0,-1,1};
+
+        for(int k=0;k<4;k++)
+        {
+            int nrow = i+row[k];
+            int ncol = j+col[k];
+
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]!=0 && !vis[nrow][ncol] )
+            {
+                dfs(grid , nrow , ncol , vis ,count);
+            }
+
+        }
+        return;
+
+    }
+    int countIslands(vector<vector<int>>& grid, int k) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>vis(m,vector<int>(n,0));
+
+        int count = 0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]!=0 && !vis[i][j])
+                {
+                    long long cnt=0;
+                    dfs(grid , i , j , vis ,cnt);
+                    if(cnt%k ==0 ) count++;
+                    
+                }
+
+            }
+        }
+        return count;
+    }
+};
