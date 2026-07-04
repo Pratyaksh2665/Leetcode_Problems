8e92246f1227b514b28acc428659273a32c9c846
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-07-04 15:57:41 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.76%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid ,int i,int j ,vector<vector<int>>&vis)
+    {
+        int m=grid.size();
+        int n=grid[0].size();
+        if(vis[i][j]) return;
+        vis[i][j]=1;
+
+        int row[4]={-1,1,0,0};
+        int col[4]={0,0,-1,1};
+
+        for(int k=0;k<4;k++)
+        {
+            int nrow = i+row[k];
+            int ncol = j+col[k];
+
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]=='X' && !vis[nrow][ncol] )
+            {
+                dfs(grid , nrow , ncol , vis);
+            }
+
+        }
+        return;
+
+    }
+    int countBattleships(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>vis(m,vector<int>(n,0));
+
+        int count = 0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]=='X' && !vis[i][j])
+                {
+                    dfs(grid , i , j , vis);
+                    count++;
+                }
+
+            }
+        }
+        return count;
+    }
+};
