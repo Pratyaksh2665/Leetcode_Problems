830a6c1fe192fb4e2de011f008d14d97d5939f5d
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-07-29 14:49:25 +0530

    Time: 7 ms (13.67%), Space: 10.9 MB (7.44%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int n=s.length();
+        for(int i=0;i<n/2;i++)
+        {
+            if(s[i]==s[n-i-1])
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+};
