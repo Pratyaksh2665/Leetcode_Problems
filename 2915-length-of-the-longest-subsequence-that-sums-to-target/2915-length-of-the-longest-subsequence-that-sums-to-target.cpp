f482c94452ba285b68585a372c1ee695594b595a
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-08-07 19:20:00 +0530

    Time: 152 ms (79.19%), Space: 156.7 MB (69.9%) - LeetHub

--- 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
+++ 2915-length-of-the-longest-subsequence-that-sums-to-target/2915-length-of-the-longest-subsequence-that-sums-to-target.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int target,int idx,vector<vector<int>>&dp)
-    {
-        if(target==0) return 0;
-        if(idx>=nums.size()) return INT_MIN;
+    // int helper(vector<int>&nums,int target,int idx,vector<vector<int>>&dp)
+    // {
+    //     if(target==0) return 0;
+    //     if(idx>=nums.size()) return INT_MIN;
 
-        if(dp[idx][target]!=-1) return dp[idx][target];
+    //     if(dp[idx][target]!=-1) return dp[idx][target];
         
-        int skip=helper(nums,target,idx+1,dp);
-        if(nums[idx]>target) return dp[idx][target]=skip;
-        int take=1+helper(nums,target-nums[idx],idx+1,dp);
+    //     int skip=helper(nums,target,idx+1,dp);
+    //     if(nums[idx]>target) return dp[idx][target]=skip;
+    //     int take=1+helper(nums,target-nums[idx],idx+1,dp);
 
-        return dp[idx][target]=max(take,skip);
-    }
+    //     return dp[idx][target]=max(take,skip);
+    // }
 
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-
-        int x=helper(nums,target,0,dp);
-        if(x>0) return x;
-        return -1;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(target + 1, INT_MIN));
+        for(int i=0;i<n;i++)dp[i][0]=0;
         
+        if(nums[0]<=target)dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                int skip = dp[i-1][j];
+
+                int take = INT_MIN;
+
+                if(nums[i]<=j && dp[i-1][j-nums[i]]!=INT_MIN)
+                    take = 1 + dp[i-1][j-nums[i]];
+
+                dp[i][j]=max(skip,take);
+            }
+        }
+
+        return dp[n-1][target]==INT_MIN ? -1 : dp[n-1][target];
     }
 };
