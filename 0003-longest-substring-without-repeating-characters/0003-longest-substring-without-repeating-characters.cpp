edf23fc63fe1166cd294286ad81db3f51ce16de2
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-08-31 20:26:13 +0530

    Time: 129 ms (22.75%), Space: 39.7 MB (21.64%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,62 +1,30 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int n=s.length();
-//         int count=0;
-//         unordered_map<char,int>mp;
-//         int i=0,j=0,mx=0;
-//         while(j<n)
-//         {
-//             mp[s[j]]++; // ye to sbke liye hoga
-//             count++;
-
-//             while(count > mp.size())
-//             {
-//                 mp[s[i]]--;
-//                 count--;
-//                 if(mp[s[i]]==0)
-//                 {
-//                      mp.erase(s[i]);
-                     
-//                 }
-//                 i++;
-//             }
-//             if(count==mp.size())
-//             {
-//                 mx=max( mx,j-i+1);
-                
-                   
-//             }
-//             j++;
-//         }
-//         return mx;
-
-        
-//     }
-// };
-
-// m2 - more clear
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
-        unordered_map<char,int> mp;
-        int i = 0, mx = 0;
-
-        for(int j = 0; j < s.size(); j++)
+        int n=s.length();
+        unordered_map<char,int>mp;
+        int i=0;
+        int j=0;
+        int maxm=0;
+        while(j<n)
         {
             mp[s[j]]++;
-
-            // if duplicate appears, shrink
-            while(mp[s[j]] > 1)
+            if(j-i+1 == mp.size())
             {
-                mp[s[i]]--;
-                i++;
+                maxm = max(maxm , j-i+1);
+
             }
-
-            mx = max(mx, j - i + 1);
+            else
+            {
+                while(mp.size()!=j-i+1)
+                {
+                    mp[s[i]]--;
+                    if(mp[s[i]]==0)mp.erase(s[i]);
+                    i++;
+                }
+            }
+            j++;
         }
-
-        return mx;
+        return maxm;
     }
 };
