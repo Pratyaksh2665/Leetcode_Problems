eb53883ccac6be5210910280cbc8ad4eac2b80c5
Pratyaksh Mishra <mishrapratyaksh82@gmail.com>
2026-09-08 19:12:44 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int total=0;
+        if(n<1000) return 0;
+        else
+        {
+
+            return n-999;
+        }
+        return 0;
+        
+    }
+};
